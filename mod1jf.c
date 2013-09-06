2dd3c92d97cd6dd21598ed371936e009f2d6b6f3
Jfergus7 <Jfergus7@mix.wvu.edu>
2013-09-05 21:52:22 -0400

    Update mod1jf.c
    ls works now

--- mod1jf.c
+++ mod1jf.c
@@ -20,64 +20,72 @@ void commhand(){
 	int exit = 0;
 	int retValue,i, command;
 	char *token;
-    char comm [4][50];  
+	char comm [2][50];
 	while(!exit){
 		comm[0][0] = '\0';
 		comm[1][0] = '\0';
-		comm[2][0] = '\0';
-		comm[3][0] = '\0';
+
 		sys_req(WRITE,TERMINAL,prompt, &pro);	
 		retValue=sys_req(READ,TERMINAL,buff, &max);
 		token = strtok(buff, "\n");
 		token = strtok(token, " ");
 		i = 0;
 		while(token != NULL){
-			strcpy(comm[i], token); 
+			strcpy(comm[i], token);
 			token = strtok(NULL, " ");
 			i++;
 		}
-	
-		command = match(comm[0]);
-		
-		switch(command){
-			case EXIT: //exit
-				exit = 1;
-				break;
-			case HELP: //help
-				if(i = 0){
-					help(-1); // no arguments
-				}else if(i = 1){
-					help(match(comm[1])); // an argument
-				}else{
-				 printf("Too many arguments,HELP accepts 0 or 1 arguments\n\n");
-				}				
-				break;
-			case VERSION: //version
-				version();
-				break;
-			case DATE: //display date
-				displayDate();
-				break;
-			case SET_DATE: //set date
-				setDate(comm[1], comm[2], comm[3]); // Date is entered like DD MM YYYY
-				break;
-			case LS: //display directory
-				if(i = 1){
-				 ls(NULL);
-				}else if(i = 2){
-				 ls(comm[1]);
-				}else{
-					printf("Too many arguments, LS accepts 0 or 1 arguments\n\n");
-				}	
-				break;
 
-			default:
-				printf("That is not a valid command.\n\n");
+	command = match(comm[0]);
+
+	switch(command){
+		case EXIT: //exit
+		exit = 1;
+		break;
+		case HELP: //help
+			if(i == 1){
+			printf("\n%d\n",i);
+				help(-1); // no arguments
+		}else if(i == 2){
+		printf("\n%d\n",i);
+			help(match(comm[1])); // an argument
+		}else{
+			printf("\n%d\n",i);
+			printf("\nToo many arguments,HELP accepts 0 or 1 arguments\n\n");
+		}	
+		break;
+		case VERSION: //version
+			version();
+			break;
+		case DATE: //display date
+			displayDate();
+			break;
+		case SET_DATE: //set date
+			if(i == 1){
+				printf("\nSETDATE requires 1 argument formatted DD/MM/YY\n\n");
+			}else
+				setDate(comm[1]); // Date is entered like DD/MM/YYYY
 				break;
+		case LS: //display directory
+			if(i == 1){
+				ls(NULL);
+			}else if(i == 2){
+				ls(comm[1]);
+			}else{
+				printf("Too many arguments, LS accepts 0 or 1 arguments\n\n");
+			}	
+			break;
+
+		default:
+			printf("That is not a valid command.\n\n");
+			break;
 		}
 	}
-	
+
 }
+
+
+
 
 int match(char * command){
 	int c = -1;
@@ -140,12 +148,40 @@ void displayDate(){
 	printf("\nThe date will be displayed here\n\n");
 }
 
-void setDate(char* d, char* m, char *y){
-	printf("\nThe Date will be set here to %s %s %s\n\n",d,m,y);
+void setDate(char* s){
+	printf("\nThe Date will be set here to %s \n\n",s);
 }
 
 void ls(char * path){
-	printf("\nThe display directory will be here\n\n");
+	char pathName[50];
+	char nameBuff[15];
+	int buffSize = 15;
+	long fileSize;
+	int status =0;
+	int count =0;
+	//open current directory 
+	if(path == NULL){
+		sys_open_dir("\0"); 
+	}else{
+		sys_open_dir(path);
+	}
+	while(TRUE){
+		status = sys_get_entry(nameBuff, buffSize, &fileSize);
+		if(status != ERR_SUP_NOENTR){
+			printf("%s.mpx\t%d\n", nameBuff, fileSize);
+			count++;
+		}else{
+			if (count == 0){
+				printf("\nTHERE ARE NO MPX FILES IN %s\n\n", path);
+			}
+			break;
+		}
+		
+	}
+	
+	sys_close_dir();
+        	        
+
 }
 
 
